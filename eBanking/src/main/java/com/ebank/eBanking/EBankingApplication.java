5bb378293415f92e26117319404adc976c2cd9ac
Bala Chander <digumarthi.bala@gmail.com>
2020-01-30 19:58:18 +0800

    Update EBankingApplication.java

--- eBanking/src/main/java/com/ebank/eBanking/EBankingApplication.java
+++ eBanking/src/main/java/com/ebank/eBanking/EBankingApplication.java
@@ -2,6 +2,7 @@ package com.ebank.eBanking;
 
 import java.util.Arrays;
 
+import com.ebank.eBanking.security.JWTAuthorizationFilter;
 import org.hibernate.annotations.Loader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,10 +13,15 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 
@@ -40,6 +46,18 @@ public class EBankingApplication{
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(EBankingApplication.class, args);	
 		ProfilesTest profilesTest = new ProfilesTest();
-		System.out.println(profilesTest.getAppName());
+	}
+
+	@EnableWebSecurity
+	@Configuration
+	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable()
+					.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+					.authorizeRequests()
+					.antMatchers(HttpMethod.POST, "/ebank/user").permitAll()
+					.anyRequest().authenticated();
+		}
 	}
 }
